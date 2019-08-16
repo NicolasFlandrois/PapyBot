b2db7082be9cf11c404e2dde3ca71dd960e45cc5
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-08-16 15:41:44 +0200

    initialised files needed for a Flask Server "Hello World"
    Flask run.py is operationnal... Initial setup in "Hello World". Server config will depart from the Hello world temple as it moves on

---
+++ papybot/models.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.7
+# UTF8
+# Date: Fri 16 Aug 2019 15:38:27 CEST 
+# Author: Nicolas Flandrois
+
+
---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.7
+# UTF8
+# Date: Fri 16 Aug 2019 15:39:17 CEST 
+# Author: Nicolas Flandrois
+
+
+from papybot import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
