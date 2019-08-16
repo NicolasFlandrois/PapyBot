77a0f42b648bab88199d024f0eaabc7adb1f8e31
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-08-16 20:47:19 +0200

    Rearranged folders to fit Flasks' Specifications & Worked on index.html
    - Flask needs a specific folder tree arrangement in order to work out.
    - index.html, needed few corrections in order to work out with those flask spec, especially for images.
    - Also in index.html I worked out the Bootstrap, integration of css/bootstrap spec inside each balise, no need for <style></style> balise as global css style ref.
    - index.html, removed from info all hyperlinks.

--- papybot/views.py
+++ papybot/views.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3.7
 # UTF8
-# Date: Fri 16 Aug 2019 15:38:48 CEST 
+# Date: Fri 16 Aug 2019 15:38:48 CEST
 # Author: Nicolas Flandrois
 
 
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello world !"
-
-if __name__ == "__main__":
-    app.run()
--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3.7
 # UTF8
-# Date: Fri 16 Aug 2019 15:39:17 CEST 
+# Date: Fri 16 Aug 2019 15:39:17 CEST
 # Author: Nicolas Flandrois
 
+from flask import Flask, render_template
 
-from papybot import app
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
