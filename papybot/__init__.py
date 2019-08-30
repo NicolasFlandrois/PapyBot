d294d87c469cef33ea56624d3d50dbedb7681b7c
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-08-30 23:22:09 +0200

    Updated path of imported files due to renaming and folders new structure

--- papybot/__init__.py
+++ papybot/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3.7
 # UTF8
-# Date: Fri 16 Aug 2019 15:38:04 CEST 
+# Date: Fri 16 Aug 2019 15:38:04 CEST
 # Author: Nicolas Flandrois
 
 
 from flask import Flask
-from .views import app
+from server import app
