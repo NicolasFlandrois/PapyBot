b24c00b9d5281cac1a30b27a3a07848067c87912
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-08-30 23:21:16 +0200

    Simplified the testing process, all tests in 1 file

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-from controls import parser
+from papybot.controls import parser, randomchat, openjson
+import json
+
+
+# def test_openjson(monkeypath):
+#     requests_mock.get('test.json', text={'data':['1', '2']})
+#     assert type(openjson()) == dict
+# Mock test create a controljson file to assert
 
 def test_parser():
     assert parser("Aucun AUTRES que Londres?") == "londres"
+
+
+def test_randomchat():
+    test_list = ["chocolat", "Vanille", "oui", "non"]
+    assert type(randomchat(test_list)) == str
