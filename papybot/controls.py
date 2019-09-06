b300e9bf33508018d22f4fcb1e89c74595c5b404
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-06 15:20:46 +0200

    Created Class Structure + Correction + Code
    Created Class Structure:
        - Papy > Managing all papy Chat, parsing
        - Api > Managing API requests
    corrected :
        - openjson
        - parser
        - randomChat
    coded : wikipedia

--- papybot/controls.py
+++ papybot/controls.py
@@ -5,66 +5,70 @@
 
 import json
 import random
-
-# Create Class 'Papy', grouping OpenJson, Parser, randomChat, and StatusParser
-def openjson(path):
-    """Json file reader, returning it's content in pure form."""
-    with open(path, 'r') as f:
-        text = json.load(f)
-    return text
+import wikipedia
 
 
-def parser(msg:str):
-    """
-    Given a string, this function will split the string into a set of words,
-    and then
-    """
+class Papy(object):
+    """Papy, grouping OpenJson, Parser, randomChat, and StatusParser"""
 
-    ignore_set = set(openjson('data.json')["parser"])
-    msg_set =  set(msg.lower().replace(".", "").replace(",", "").replace("?", "")
-                .replace(";", "").replace(":", "").replace("!", "").split())
-
-    return " ".join(msg_set.difference(ignore_set))
+    def openjson(path):
+        """Json file reader, returning it's content in pure form."""
+        with open(path, 'r') as f:
+            text = json.load(f)
+        return text
 
 
-def randomchat(lstmsg:list, status):
-    """Randomly return a string item from a list of strings."""
-        # def statusParser():
-        # """Get a status from the status tracker, returns a list accordingly"""
-        # Fails = 0; msg = 1; Greating = 2
-            # if status is 0:
-            #   use fails message
-            # elif status is 1:
-            #   Use msg
-            # else :
-            #   use Greetings
-    # if status is 0:
-    #   use fails message
-    # elif status is 1:
-    #   Use msg
-    # else :
-    #   use Greetings
-    return random.choice(lstmsg)
+    def parser(msg:str, path):
+        """
+        Given a string, this function will split the string into a set of words,
+        and then
+        """
+
+        ignore_set = set(Papy.openjson(path)["parser"])
+        msg_set =  set(msg.lower().replace(".", "").replace(",", "").replace("?", "")
+                    .replace(";", "").replace(":", "").replace("!", "").split())
+
+        return " ".join(msg_set.difference(ignore_set)).capitalize()
 
 
-    # def StatusTracker():
+    def randomchat(lstmsg:list, status):
+        """Randomly return a string item from a list of strings."""
+            # def statusParser():
+            # """Get a status from the status tracker, returns a list accordingly"""
+            # Fails = 0; msg = 1; Greating = 2
+                # if status is 0:
+                #   use fails message
+                # elif status is 1:
+                #   Use msg
+                # else :
+                #   use Greetings
+        # if status is 0:
+        #   use fails message
+        # elif status is 1:
+        #   Use msg
+        # else :
+        #   use Greetings
+        return random.choice(lstmsg)
 
-# Create class 'Search engine' or 'API'
-    # def wikisearch(searchObject):
-    '''Given a string remotely search in wiki what's needed'''
-    # Try:
-        # from wikipedia import summary, page, set_lang
-        # First set langue to french with: wikipedia.set_lang("fr")
-        # stock search variable in a var 'city' or called 'seachObject'
-        # summary(city, sentences=2)
-        # page(city).url
-            ## NB: No Way around it.
-            # The function wikipedia.page().content cannot split a summary of n sentences
-            # And Summary cannot give the url
-    # Except:
-        # Change status to Fails
-        # If "wikipedia.exceptions.DisambiguationError" Possibly invit user to precise his query? e.g. (test lang:en)"roma"
-            # return '\nMerci de redéfinir la question, plus précisément. (e.g. Ajoute un pays)\nJe te propose : ' wikipedia.search(searchObjet)
+
+        # def StatusTracker():
+
+    # Create class 'Search engine' or 'API'
+        # def wikisearch(searchObject):
+        '''Given a string remotely search in wiki what's needed'''
+        # Try:
+            # from wikipedia import summary, page, set_lang
+            # First set langue to french with: wikipedia.set_lang("fr")
+            # stock search variable in a var 'city' or called 'seachObject'
+            # summary(city, sentences=2)
+            # page(city).url
+                ## NB: No Way around it.
+                # The function wikipedia.page().content cannot split a summary of n sentences
+                # And Summary cannot give the url
+        # Except:
+            # Change status to Fails
+            # If "wikipedia.exceptions.DisambiguationError" Possibly invit user to precise his query? e.g. (test lang:en)"roma"
+                # return '\nMerci de redéfinir la question, plus précisément. (e.g. Ajoute un pays)\nJe te propose : ' wikipedia.search(searchObjet)
 
 
 class Api(object):
@@ -78,11 +82,9 @@ class Api(object):
         If not found, or exception raised, an error message will be displaid.
         """
         try:
-            return f'{wikipedia.summary(request)} \
-            \n\n {wikipedia.page(request).url}'
+            return f'{wikipedia.summary(request)}\n\n{wikipedia.page(request).url}'
         except:
-            return f'\nMerci de redéfinir la question, plus précisément. \
-(e.g. Ajoute un pays)\nJe te propose : {i for i in wikipedia.search(request)}'
+            return f"Merci de redéfinir ta question, plus précisément. (e.g. Ajoute un pays)\nJe te propose : {', '.join(set([i for i in wikipedia.search(request)]))}"
 
 
     # def GoogleMap(searchObject):
