c72bdb0ce696d24bea62a440656858eb6be671cd
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-06 15:16:59 +0200

    Implemented and corrected some Unit Testings
    test_openjson
    test_randomchat
    test_wikipedia

--- test.py
+++ test.py
@@ -1,36 +1,27 @@
 from papybot.controls import *
+from papybot.models import *
+from papybot.views import *
+from server import *
 import json
 
 
-# def test_openjson(monkeypath):
-#     requests_mock.get('test.json', text={'data':['1', '2']})
-#     assert type(openjson()) == dict
-# Mock test create a controljson file to assert
+def test_openjson():
+    assert type(Papy.openjson('./papybot/data.json')) == dict
+
 
 def test_parser():
-    assert parser("Aucun AUTRES que Londres?") == "londres"
+    assert Papy.parser("Aucun AUTRES que Londres?", './papybot/data.json') == "Londres"
 
 
 def test_randomchat():
     test_list = ["chocolat", "Vanille", "oui", "non"]
-    assert type(randomchat(test_list, 0)) == str
+    assert type(Papy.randomchat(test_list, 0)) == str
+
 
 def test_wikipedia():
-    assert type(wikipedia('London')) == tuple
-    assert wikipedia('french_republican_date')[0] == \
-        "The French Republican calendar (French: calendrier républicain \
-français), also commonly called the French Revolutionary calendar \
-(calendrier révolutionnaire français), was a calendar created and implemented \
-during the French Revolution, and used by the French government for about 12 \
-years from late 1793 to 1805, and for 18 days by the Paris Commune in 1871. \
-The revolutionary system was designed in part to remove all religious and \
-royalist influences from the calendar, and was part of a larger attempt at \
-decimalisation in France (which also included decimal time of day, \
-decimalisation of currency, and metrication). It was used in government \
-records in France and other areas under French rule, including Belgium, \
-Luxembourg, and parts of the Netherlands, Germany, Switzerland, Malta, and \
-Italy.\
-\
-https://en.wikipedia.org/wiki/French_Republican_calendar"
-    assert wikipedia('zsecfu')[0] == 'Information not found.'
-    assert wikipedia('zsecfu')[1] == 'Information not found.'
+    assert type(Api.wikipedia('London')) == str
+    assert Api.wikipedia('french_republican_date') == "The French Republican calendar (French: calendrier républicain français), also commonly called the French Revolutionary calendar (calendrier révolutionnaire français), was a calendar created and implemented during the French Revolution, and used by the French government for about 12 years from late 1793 to 1805, and for 18 days by the Paris Commune in 1871. The revolutionary system was designed in part to remove all religious and royalist influences from the calendar, and was part of a larger attempt at decimalisation in France (which also included decimal time of day, decimalisation of currency, and metrication). It was used in government records in France and other areas under French rule, including Belgium, Luxembourg, and parts of the Netherlands, Germany, Switzerland, Malta, and Italy.\n\nhttps://en.wikipedia.org/wiki/French_Republican_calendar"
+    assert Api.wikipedia('zsecfu') == 'Merci de redéfinir ta question, plus précisément. (e.g. Ajoute un pays)\nJe te propose : '
+
+# def test_gmap():
+    # assert
