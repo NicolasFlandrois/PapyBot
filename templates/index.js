5893f9d285585d52f02eefa0a59f488aaeb3cf15
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-13 15:12:19 +0200

    Commented stages of development
    What needs to get in there

--- templates/index.js
+++ templates/index.js
@@ -2,10 +2,10 @@
 // Date : Thu 15 Aug 2019 18:51:34 CEST
 // Javascript & Ajax & Jquery
 
-// Get question request from HTML
+// Get question request from HTML (AJAX/JQuery)
 
-// Post question request to Python
+// Post question request to Python Flask Server (AJAX/JQuery)
 
-// Get Responce from Python
+// Get Responce from Python Flask Server (AJAX/JQuery)
 
-// Post Responce to HTML
+// Post Responce to HTML > Use a look like a text messenger round shape & distinguish Human (User) from PapyBot (Computer) (AJAX/JQuery)
