4b5caa637939015aadae3d66eb737adab55f0d66
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-13 15:16:56 +0200

    Commenting next dev steps on server.py

--- server.py
+++ server.py
@@ -13,6 +13,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# Get request/input from HTML/JS form & send to/use controls.py
+# Get controls Reply: Sucess/Failure + Reply message & Send to JS to display
+# IF Previous step == success Get parsed message, & send to Gmap API in JS, Else None
+
+
 # @app.route('/message/<str:msg>')
 # def msg(msg):
 #     return #return truc bidon pour test puis changer avec API plus tard
