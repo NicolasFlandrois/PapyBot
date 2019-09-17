14b29cea05b30b15a787d628cf43e60ca8eef9d6
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-17 21:11:29 +0200

    Dialog Box Display Adjustments using Bootstrap
    Using Bootstrap, defined areas for dialog to take place, and shape and colour. 
    
    Jquery == Fails

--- static/index.js
+++ static/index.js
@@ -2,32 +2,32 @@
 // Date : Thu 15 Aug 2019 18:51:34 CEST
 // Javascript & Ajax & Jquery
 
-
-// Get question request from HTML (AJAX/JQuery)
-
-// Post question request to Python Flask Server (AJAX/JQuery)
-
-// Get Responce from Python Flask Server (AJAX/JQuery)
-
-// Post Responce to HTML > Use a look like a text messenger round shape & distinguish Human (User) from PapyBot (Computer) (AJAX/JQuery)
-
-// Use Bootstrap Float right or left for each messenger exchanges cf https://getbootstrap.com/docs/4.2/utilities/float/
-// Use Bootstrap Borders to encapsulate messengers cf https://getbootstrap.com/docs/4.2/utilities/borders/
 $(()=> {
+    // Get question request from HTML (AJAX/JQuery)
     $("#user_post").on("#submit_btn", ()=> {
-        let el = $("#user_post");
-        let url = "http://127.0.0.1:5000/message/" + el.val() + "/";
+        let el1 = $("#user_post");
+
+        let el1 = "<p class='d-flex justify-content-center p-2 float-left rounded-pill border-right border-bottom border-secondary bg-primary align-middle text-white'>" + el1 + "</p>";
+        console.log(el1);
+        question = $("#userSide");
+        question.html("");
+        question.append(el1);
+
+        // Post question request to Python Flask Server (AJAX/JQuery)
+        let url = "http://127.0.0.1:5000/message/" + el1.val() + "/";
+        // Get Responce from Python Flask Server (AJAX/JQuery)
         $.get(url, (res)=> {
             let words = JSON.parse(res);
-            let container = $(".dialogbox");
-            container.html("");
+            let results = $("#papySide");
+            results.html("");
             // Do sthg here
             // Here goes :     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
-            for( word of words) {
-                let el = "<p>" + word + "</p>";
-                console.log(el);
-                container.append(el);
-            }
+
+            let el2 = "<p class='d-flex justify-content-center p-2 float-left rounded-pill border-left border-bottom border-secondary bg-success align-middle text-white'>" + words + "</p>";
+            console.log(el2);
+            // Post Responce to HTML
+            results.append(el2);
+
         })
-    });
+    };
 });
