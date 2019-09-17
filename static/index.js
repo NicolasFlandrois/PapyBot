0c59e565c714af26a369ed826940d7d1ddc847d6
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-18 00:30:50 +0200

    Jquery Variables Standardisation

--- static/index.js
+++ static/index.js
@@ -3,31 +3,31 @@
 // Javascript & Ajax & Jquery
 
 $(()=> {
-    // Get question request from HTML (AJAX/JQuery)
-    $("#user_post").on("#submit_btn", ()=> {
-        let el1 = $("#user_post");
+            // Get question request from HTML (AJAX/JQuery)
+            $("#user_post").on("#submit_btn", ()=> {
+                let request = $("#user_post");
 
-        let el1 = "<p col-7 d-flex justify-content-center p-3 float-left rounded-right border-right border-bottom border-secondary shadow-sm bg-primary align-middle text-left text-white mr-auto'>" + el1 + "</p>";
-        console.log(el1);
-        request = $("#dialogBox");
-        request.html("");
-        request.append(el1);
+                let reqDisplay = "<p col-7 d-flex justify-content-center p-3 float-left rounded-right border-right border-bottom border-secondary shadow-sm bg-primary align-middle text-left text-white mr-auto'>" + el1 + "</p>";
+                console.log(reqDisplay);
+                request = $("#dialogBox");
+                request.html("");
+                request.append(el1);
 
-        // Post question request to Python Flask Server (AJAX/JQuery)
-        let url = "http://127.0.0.1:5000/message/" + el1.val() + "/";
-        // Get Responce from Python Flask Server (AJAX/JQuery)
-        $.get(url, (res)=> {
-            let words = JSON.parse(res);
-            let results = $("#dialogBox");
-            results.html("");
-            // Do sthg here
-            // Here goes :     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+                // Post question request to Python Flask Server (AJAX/JQuery)
+                let url = "http://127.0.0.1:5000/message/" + el1.val() + "/";
+                // Get Responce from Python Flask Server (AJAX/JQuery)
+                $.get(url, (res)=> {
+                    let words = JSON.parse(res);
+                    let results = $("#dialogBox");
+                    results.html("");
+                    // Do sthg here
+                    // Here goes :     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
 
-            let el2 = "<p class='col-7 d-flex justify-content-center p-3 float-right rounded-left border-left border-bottom border-secondary shadow-sm bg-success align-middle text-left text-white mr-auto'>" + words + "</p>";
-            console.log(el2);
-            // Post Responce to HTML
-            results.append(el2);
+                    let el2 = "<p class='col-7 d-flex justify-content-center p-3 float-right rounded-left border-left border-bottom border-secondary shadow-sm bg-success align-middle text-left text-white mr-auto'>" + words + "</p>";
+                    console.log(el2);
+                    // Post Responce to HTML
+                    results.append(el2);
 
-        })
-    };
-});
+                    })
+                };
+        });
