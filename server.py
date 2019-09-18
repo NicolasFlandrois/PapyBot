dfe7f583c0b29b06df1ce7d5c790dc86a32312d1
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-18 16:10:14 +0200

    AJAX/Flask's Fetch/JSON transmission Cient-server Attempt Failed

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 # Date: Fri 16 Aug 2019 15:39:17 CEST
 # Author: Nicolas Flandrois
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
 import json
 
 
@@ -15,15 +15,25 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/message/<msg>')
-def msg(msg):
-    # Get request/input from HTML/JS form & send to/use controls.py
-    # Get controls Reply: Sucess/Failure + Reply message & Send to JS to display
-    # IF Previous step == success Get parsed message, & send to Gmap API in JS, Else None
+# @app.route('/message/<msg>')
+# def msg(msg):
+#     # Get request/input from HTML/JS form & send to/use controls.py
+#     # Get controls Reply: Sucess/Failure + Reply message & Send to JS to display
+#     # IF Previous step == success Get parsed message, & send to Gmap API in JS, Else None
 
-    # return json.dumps(output)#return json file... to transmit to client cf how I did it in tests
-    return json.dumps({'Hello World'})    # Test Dev
+#     # return json.dumps(output)#return json file... to transmit to client cf how I did it in tests
+#     return json.dumps({'Hello World'})    # Test Dev
 
+@app.route("/message", methods=["GET"])
+def create_entry():
+
+    req = request.get_json()
+
+    print(req)
+
+    res = make_response(jsonify({"message": "OK"}), 200)
+
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
