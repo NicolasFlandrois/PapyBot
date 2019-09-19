d54eaace71dd01f95286ddf3adee3d052d80b1fa
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-19 21:51:10 +0200

    Verif JS
    Cleaned what makes no sens to me.
    Testing manually each component, step by step
    
    kept the AJAX connection, 
    and the get() function listening to the server
    
    >>> Missing<<<<
    Missing a function to get from HTML, and send, once button submit is clicked

--- static/index.js
+++ static/index.js
@@ -2,24 +2,28 @@
 // Date : Thu 15 Aug 2019 18:51:34 CEST
 // Javascript & Ajax & Jquery
 
-// $.ajax({
-//         type: "GET",
-//         url: `${window.origin}/message`,
-//         contentType: "application/json",
-//         dataType: "json",
-//         data: JSON.stringify({
-//             message: $("#user_request").val()
-//         }),
+// Sending the request to server
+$.ajax({
+        type: "GET",
+        url: `${window.origin}/message/`,
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+            message: $("#user_request").val()
+        }),
 
-//         success: function(response) {
-//             console.log(response);
-//         },
-//         error: function(response) {
-//             console.log(response);
-//         }
-// });
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(response) {
+            console.log(response);
+        }
+});
 
-// $.get(`${window.origin}/message`, {category:'client', type:'premium'})
+// Manque une fonction qui à validation btn-submit envoie la requete @ serveur
+
+// Receiving the response from to server
+// $.get(`${window.origin}/message/`, {category:'client', type:'premium'})
 //      .done(function(response) {
 //            $("#dialogBox").html(response.amount);
 //      });
@@ -30,27 +34,3 @@
 // PapyBot Response should be incorporate/interpolate in:
 // "<p class='col-7 d-flex justify-content-center p-3 float-right rounded-left border-left border-bottom border-secondary shadow-sm bg-success align-middle text-left text-white mr-auto'>" + words + "</p>"
 
-$(()=> {
-
-    $("#submit_btn").click( submit_entry() {
-
-        let container = $("#dialogBox");
-
-        let el = $("#user_request");
-        console.log(el);
-        let url = "${window.origin}/message/" + el.val() + "/";
-        console.log(url);
-
-        // $.get(url, (res)=> {
-
-        //     let words = JSON.parse(res);
-        //     container.html("");
-        //     let el = "<p> PAPY" + words + "</p>";
-        //     console.log(el);
-        //     container.append(el);
-
-        // })
-
-    });
-
-});
