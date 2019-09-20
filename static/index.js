6cfab06dd7c621d8e21256bd2a135c038303b8b1
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-20 15:32:26 +0200

    Jquery - Attempt - Not Successfull
    I managed to create something that makes sense to me, in JS file.
    
    JS should fetch on submit's click the input field content, store in a variable, create a url in GET...
    Send the info to the server.
    Then wait for the reply, and display the reply's content in the dialogbox area.
    
    However this doesn't works.
    
    Direct input in the nav browser, works.
    So server works.
    
    It seems that I don't send what's requiered. As after clicking submit, the adress bar in the browser returns:
    http://127.0.0.1:5000/?user_request=pouloulouloulou&submit=
    
    instead of : http://127.0.0.1:5000/msg/pouloulou%20du%20pouloulou

--- static/index.js
+++ static/index.js
@@ -10,3 +10,30 @@
 // PapyBot Response should be incorporate/interpolate in:
 // "<p class='col-7 d-flex justify-content-center p-3 float-right rounded-left border-left border-bottom border-secondary shadow-sm bg-success align-middle text-left text-white mr-auto'>" + words + "</p>"
 
+$(function() {
+    $('#submit_btn').on('click', function() {
+
+        let container = $("#dialogBox");
+        container.html("");
+
+        let req = $("#user_request");
+        let url = "http://127.0.0.1:5000/msg/" + req.val() + "/";
+
+        container.append(req);
+
+        console.log(req);
+        console.log(url);
+
+      $.get(url,
+
+            (res)=> {
+            let words = JSON.parse(res);
+            console.log(words)
+
+            let results = "<p>" + words + "</p>";
+            console.log(results);
+            container.append(results);
+        });
+
+    });
+});
