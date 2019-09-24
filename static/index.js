f66ef4e688229a09ec122418c95fbabb41086792
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-24 21:02:57 +0200

    GET input and print in console.log = success
    return in console.log = user_request=Hello%20Wolrd
    The input has been Jsonify, then Stingify to send it to server.

--- static/index.js
+++ static/index.js
@@ -10,55 +10,9 @@
 // PapyBot Response should be incorporate/interpolate in:
 // "<p class='col-7 d-flex justify-content-center p-3 float-right rounded-left border-left border-bottom border-secondary shadow-sm bg-success align-middle text-left text-white mr-auto'>" + words + "</p>"
 
-// $(function() {
-//     $('form').on('submit', function(event) {
 
-
-
-//         let container = $("#dialogBox");
-//         container.html("");
-
-//         let req = $("#user_request");
-//         let url = "http://127.0.0.1:5000/msg/" + req.val() + "/";
-
-//         let usrsays = "<p>" + words + "</p>";
-//         container.append(usrsays);
-
-//         console.log(usrmsg);
-//         console.log(req);
-//         console.log(url);
-
-//       $.get(url,
-
-//             (res)=> {
-//             let words = JSON.parse(res);
-//             console.log(words)
-
-//             let papysays = "<p>" + words + "</p>";
-//             console.log(papysays);
-//             container.append(papysays);
-//         });
-
-//         event.preventDefault();
-
-//     });
-// });
-
-
-$(document).ready(function() {
-    $('form').on('submit', function(event) {
-
-        $.ajax({
-            data : {msg : $('#user_request')},
-            type : 'GET',
-            url : '/msg'
-        })
-
-        .done(function(data) {
-            $('#dialogBox').text(data.result).show();
-        });
-
-        event.preventDefault();
-
-    });
+$( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  let myJSON = JSON.stringify($( this ).serialize());
+  console.log( $( this ).serialize() );
 });
